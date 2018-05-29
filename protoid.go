ae1f6f8ad6d65c6745f24249abba9ef20646b9b5
Martin Garton <garton@gmail.com>
2018-05-29 17:58:58 +0100

    Add reader
    This adds a reader type that encapsulates reading from a protobuf message,
    tracking position correctly and tracking error state as it goes. This makes the
    rest of the code shorter and clearer.

--- protoid.go
+++ protoid.go
@@ -19,40 +19,22 @@ var (
 // Decode decodes an arbitrary protocol buffers message into a map of field number to field value. It makes a best-effort attempt to use the most appropriate type for the values.  Embedded structs, strings, integers and more are often decoded correctly.  However due to the nature of protocol buffers, it is not always possible to do this perfectly.
 func Decode(input []byte) (map[int]interface{}, error) {
 	m := make(map[int]interface{})
-	for len(input) > 0 {
-		val, i, err := decodeVarint(input)
-		if err != nil {
-			return nil, err
-		}
-		input = input[i:]
+
+	r := &reader{buf: input}
+
+	for !r.done() {
+		val := r.decodeVarint()
 		wiretype := val & 0x07
 		k := int(val >> 3)
 		switch wiretype {
 		case 0: // varint value (int32, int64, uint32, uint64, sint32, sint64, bool, enum)
-			v, i, err := decodeVarint(input)
-			if err != nil {
-				return nil, err
-			}
-			input = input[i:]
+			v := r.decodeVarint()
 			m[k] = v
 		case 1: // 64 bit value (fixed64, sfixed64, double)
-			if len(input) < 8 {
-				return nil, ErrUnexpectedEndOfInput
-			}
-			v := binary.LittleEndian.Uint64(input)
-			input = input[8:]
+			v := r.readLeUint64()
 			m[k] = v
 		case 2: // length-delimited value (string, bytes, embedded messages, packed repeated fields)
-			l, i, err := decodeVarint(input)
-			if err != nil {
-				return nil, err
-			}
-			input = input[i:]
-
-			if uint64(len(input)) < l {
-				return nil, ErrUnexpectedEndOfInput
-			}
-			data := input[0:l]
+			data := r.readLenDelimValue()
 
 			var value interface{}
 			// try to guess the type of data
@@ -85,42 +67,104 @@ func Decode(input []byte) (map[int]interface{}, error) {
 			} else {
 				m[k] = value
 			}
-			if uint64(len(input)) < l {
-				return nil, ErrUnexpectedEndOfInput
-			}
-			input = input[l:]
 		case 3: // Start group (groups are deprecated)
 			return nil, ErrNotImplemented
 		case 4: // End group (groups are deprecated)
 			return nil, ErrNotImplemented
 		case 5: // 32-bit value (fixed32, sfixed32, float)
-			if len(input) < 4 {
-				return nil, ErrUnexpectedEndOfInput
-			}
-			val := binary.LittleEndian.Uint32(input)
-			input = input[4:]
+			val := r.readLeUint32()
 			m[k] = val
 		default:
 			return nil, fmt.Errorf("unsupported wire type : %v", wiretype)
 		}
 	}
+	if r.err != nil {
+		return nil, r.err
+	}
 	return m, nil
 }
 
-func decodeVarint(buf []byte) (val uint64, l int, err error) {
+type reader struct {
+	buf []byte
+	err error
+}
+
+func (r *reader) Err() error {
+	return r.err
+}
+
+func (r *reader) done() bool {
+	return len(r.buf) == 0 || r.err != nil
+}
+
+func (r *reader) readLeUint32() uint32 {
+	if r.err != nil {
+		return 0
+	}
+
+	if len(r.buf) < 4 {
+		r.err = ErrUnexpectedEndOfInput
+		return 0
+	}
+	v := binary.LittleEndian.Uint32(r.buf)
+	r.buf = r.buf[4:]
+	return v
+}
+
+func (r *reader) readLeUint64() uint64 {
+	if r.err != nil {
+		return 0
+	}
+
+	if len(r.buf) < 8 {
+		r.err = ErrUnexpectedEndOfInput
+		return 0
+	}
+	v := binary.LittleEndian.Uint64(r.buf)
+	r.buf = r.buf[8:]
+	return v
+}
+
+func (r *reader) readLenDelimValue() []byte {
+	if r.err != nil {
+		return nil // TODO: return empty slice?
+	}
+
+	l := r.decodeVarint()
+
+	if uint64(len(r.buf)) < l {
+		r.err = ErrUnexpectedEndOfInput
+		return nil // TODO: return empty slice?
+	}
+	data := r.buf[0:l]
+	r.buf = r.buf[l:]
+	return data
+}
+
+func (r *reader) decodeVarint() uint64 {
+	if r.err != nil {
+		return 0
+	}
+
+	var l int
+	var val uint64
 	for shift := uint(0); shift < 64; shift += 7 {
-		if l >= len(buf) {
-			return 0, 0, ErrUnexpectedEndOfInput
+		if l >= len(r.buf) {
+			r.err = ErrUnexpectedEndOfInput
+			return 0
 		}
-		b := uint64(buf[l])
+		b := uint64(r.buf[l])
 		l++
 		val |= (b & 0x7F) << shift
 		if (b & 0x80) == 0 {
-			return val, l, nil
+			r.buf = r.buf[l:]
+			return val
 		}
 	}
 
-	return 0, 0, ErrNumberTooLarge
+	r.err = ErrNumberTooLarge
+	return 0
+
 }
 
 func copyBytes(in []byte) []byte {
